de35bcf8acf24b5fafdfbf7fff09c33703e28b24
Brian Munene <97797523+dev-brianmunene@users.noreply.github.com>
2024-05-31 16:24:00 +0300

    Add files via upload

---
+++ script_1.js
@@ -0,0 +1,12 @@
+<script>
+window.embeddedChatbotConfig = {
+chatbotId: "PctUKL0x4sqqu4hgKL8re",
+domain: "www.chatbase.co"
+}
+</script>
+<script
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="PctUKL0x4sqqu4hgKL8re"
+domain="www.chatbase.co"
+defer>
+</script>
